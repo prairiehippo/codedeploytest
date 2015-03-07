6575a6fb3c7847f5fe0c467446be73e245ef995f
Boley <ryanbole@amazon.com>
2015-03-07 11:58:31 -0800

    Added new theme

---
+++ sites/all/themes/amazon/templates/page--front.tpl.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div>
+</nav>
+
+<div class="container">
+	<div id="content">
+		<img src="sites/all/themes/amazon/images/logo.png" />
+		<!-- <img src="sites/all/themes/amazon/images/goat.jpg" /> -->
+	</div>
+</div>
